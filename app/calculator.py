b1786fc4c7ef23ab49312a98223b5f6ced0e644c
Ryan Mecham <ryan.mecham@mx.com>
2024-06-22 22:36:38 -0600

    working reverse polish notation calculator implementation

--- app/calculator.py
+++ app/calculator.py
@@ -9,12 +9,14 @@ Polish notation: 4 is first subtracted from 3, then 5 is added to it.
 Supported operators include: +,-,*,/
 '''
 
-import logging
+import logging, operator
 logger = logging.getLogger(__name__) #does this define this in main.py too?
 
 class Calculator:
   def __init__(self):
     self.stack = []
+    self.current_value = None
+    logger.info("hi, I'm a new Calculator -----------------------------------")
 
   # add items to the stack
   def push(self, items):
@@ -24,7 +26,58 @@ class Calculator:
     else:
       self.stack.append(items)
 
+  ''' recursively evaluate operators
+  for example:
+  
+  1,2,3,+,-
+
+  evaluates as
+
+  -
+    +
+      1,2,3
+    1,5
+  -4
+  '''
+  def evaluate(self, operation):
+    logger.debug(f'evaluating {operation} with stack of: {self.stack}')
+
+    if len(self.stack) > 0:
+      right = self.stack.pop()
+    else:
+      raise ValueError("Invalid Stack -- Too few operands")
+    
+    # check for another operator and call it recursively
+    if callable(right):
+      right = self.evaluate(right)
+
+    if len(self.stack) > 0:
+      left = self.stack.pop()
+    else:
+      # You hit the end of the stack, grab the current_value as `left` if present
+      # this allows for multiple calls to `.calculate()` on the same instance
+      if self.current_value is None:
+        raise ValueError("Invalid Stack -- Too few operands")
+      else:
+        left = self.current_value
+
+    if callable(left):
+      left = self.evaluate(left)
+
+    #base case
+    return operation(left,right)
+
   # process the current stack
   def calculate(self):
-    logger.debug(f'caluclating stack of: {self.stack}')
-    return 0
+    operation = self.stack.pop()
+    
+    if not callable(operation):
+      raise ValueError("Invalid stack -- got operand when expected operator")
+    
+    self.current_value = self.evaluate(operation)
+
+    # if there is anything left on the stack, then the stack was bad
+    if len(self.stack) != 0:
+      raise ValueError("Invalid stack -- Too many operands in a row")
+      
+    return self.current_value
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,13 +1,14 @@
 import unittest
 import context
+import operator, logging
 from app.calculator import Calculator
 
-class TestCalculator(unittest.TestCase):
+class TestPush(unittest.TestCase):
 
     def test_push_array(self):
         calculator = Calculator()
-        calculator.push([1,2,'+'])
-        self.assertEqual(calculator.stack, [1,2,'+'], 'The stack is wrong.')
+        calculator.push([1,2,operator.add])
+        self.assertEqual(calculator.stack, [1,2,operator.add], 'The stack is wrong.')
 
     def test_push_int(self):
         calculator = Calculator()
@@ -17,9 +18,107 @@ class TestCalculator(unittest.TestCase):
     
     def test_push_operator(self):
         calculator = Calculator()
-        calculator.push('+')
-        calculator.push('-')
-        self.assertEqual(calculator.stack, ['+','-'], 'The stack is wrong.')
+        calculator.push(operator.add)
+        calculator.push(operator.sub)
+        self.assertEqual(calculator.stack, [operator.add,operator.sub], 'The stack is wrong.')
+
+class TestCalculate(unittest.TestCase):
+    def test_calculate_valid_stack_1(self):
+        calculator = Calculator()
+        calculator.push([1,2,operator.add])
+        self.assertEqual(calculator.calculate(), 3, 'incorrect result')
+
+    def test_calculate_valid_stack_2(self):
+        calculator = Calculator()
+        calculator.push([1,2,operator.add,3,operator.add])
+        self.assertEqual(calculator.calculate(), 6, 'incorrect result')
+
+    def test_calculate_multiple_push_valid_stack(self):
+        calculator = Calculator()
+        calculator.push([1,2,operator.add])
+        self.assertEqual(calculator.calculate(), 3, 'incorrect result')
+        calculator.push([3,operator.sub])
+        self.assertEqual(calculator.calculate(), 0, 'incorrect result')
+
+    def test_calculate_push_invalid_stack(self):
+        calculator = Calculator()
+        calculator.push([1,2,operator.add,3,operator.sub,4])
+        self.assertRaises(ValueError, calculator.calculate)
+
+    def test_calculate_multiple_push_invalid_stack(self):
+        calculator = Calculator()
+        calculator.push([1,2,operator.add])
+        self.assertEqual(calculator.calculate(), 3, 'incorrect result')
+        calculator.push([3,operator.sub,4])
+        self.assertRaises(ValueError, calculator.calculate)
+    
+    def test_calculate_invalid_stack_only_operands(self):
+        calculator = Calculator()
+        calculator.push([1,2])
+        with self.assertRaises(ValueError) as context:
+            calculator.calculate()
+        self.assertTrue('Invalid stack -- got operand when expected operator' in str(context.exception))
+
+    def test_calculate_invalid_stack_only_operands2(self):
+        calculator = Calculator()
+        calculator.push([1,2,3,4,5])
+        with self.assertRaises(ValueError) as context:
+            calculator.calculate()
+        self.assertTrue('Invalid stack -- got operand when expected operator' in str(context.exception))
+
+    def test_complicated_stack(self):
+        calculator = Calculator()
+        calculator.push([1,2,3,operator.add,operator.sub])
+        self.assertEqual(calculator.calculate(), -4, 'incorrect result')
+
+    def test_calculate_too_many_operands(self):
+        calculator = Calculator()
+        calculator.push([1,2,1,operator.add])
+        with self.assertRaises(ValueError) as context:
+            calculator.calculate()
+        self.assertTrue('Invalid stack -- Too many operands in a row' in str(context.exception))
+
+    def test_calculate_too_many_operands2(self):
+        calculator = Calculator()
+        calculator.push([operator.add,2,1,operator.add])
+        with self.assertRaises(ValueError) as context:
+            calculator.calculate()
+        self.assertTrue('Invalid stack -- Too many operands in a row' in str(context.exception))
+
+    def test_calculate_too_many_operands3(self):
+        calculator = Calculator()
+        calculator.push([1,operator.add,2,1,operator.add])
+        with self.assertRaises(ValueError) as context:
+            calculator.calculate()
+        self.assertTrue('Invalid stack -- Too many operands in a row' in str(context.exception))
+
+    def test_calculate_too_many_operands4(self):
+        calculator = Calculator()
+        calculator.push([1,operator.add])
+        with self.assertRaises(ValueError) as context:
+            calculator.calculate()
+        self.assertTrue('Invalid Stack -- Too few operands' in str(context.exception))
+
+    def test_all_operators(self):
+        calculator = Calculator()
+        calculator.push([operator.add,operator.add,operator.add])
+        with self.assertRaises(ValueError) as context:
+            calculator.calculate()
+        self.assertTrue('Invalid Stack -- Too few operands' in str(context.exception))
+
+    def test_too_few_operands(self):
+        calculator = Calculator()
+        calculator.push([1,2,operator.add,operator.add,operator.add])
+        with self.assertRaises(ValueError) as context:
+            calculator.calculate()
+        self.assertTrue('Invalid Stack -- Too few operands' in str(context.exception))
+
+    def test_numcom_calculation(self):
+        calculator = Calculator()
+        calculator.push([9,2,operator.mul,7,operator.add,1,operator.sub])
+        self.assertEqual(calculator.calculate(), 24, 'incorrect result')
+
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='debug.log', level=logging.DEBUG)
     unittest.main()
